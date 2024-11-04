fa0424e62683f3724046e3ca2589d40f1c4c2d0c
CyberL1 <cyber.hf18@gmail.com>
2024-11-04 21:47:24 +0100

    start rewrite

---
+++ routes/index.ts
@@ -0,0 +1,59 @@
+import { Router } from "express";
+import { getInstances, getInstance } from "../utils.ts";
+import { Domains } from "../types.ts";
+
+const router = Router();
+
+router.get("/*", async (req, res) => {
+  const host = req.get("host");
+
+  if (host === `localhost:${process.env.PORT}`) {
+    const instances = getInstances();
+
+    res.render("list", { instances, host });
+    return;
+  }
+
+  const instance = getInstance(host?.split(".")[0] as string);
+
+  if (!instance) {
+    res.send("Instance not found");
+    return;
+  }
+
+  const page = await fetch(`${Domains[instance.releaseChannel]}/${req.path}`);
+  let content = await page.text();
+
+  if (instance.domains) {
+    if (instance.domains.api) {
+      content = content.replace(
+        "API_ENDPOINT: '//canary.discord.com/api'",
+        `API_ENDPOINT: '${instance.domains.api}'`,
+      );
+    }
+
+    if (instance.domains.gateway) {
+      content = content.replace(
+        "GATEWAY_ENDPOINT: 'wss://gateway.discord.gg'",
+        `GATEWAY_ENDPOINT: '${instance.domains.gateway}'`,
+      );
+    }
+    if (instance.domains.cdn) {
+      content = content.replace(
+        "CDN_HOST: 'cdn.discordapp.com'",
+        `CDN_HOST: '${instance.domains.cdn}'`,
+      );
+    }
+
+    if (instance.domains.media) {
+      content = content.replace(
+        "MEDIA_PROXY_ENDPOINT: '//media.discordapp.net'",
+        `MEDIA_PROXY_ENDPOINT: '${instance.domains.media}'`,
+      );
+    }
+  }
+
+  res.send(content);
+});
+
+export default router;
--- utils.ts
+++ utils.ts
@@ -1,53 +1,23 @@
-import { DOMAINS, ENDPOINTS } from "./contants.ts";
-import { Build, Commit, ReleaseChannel } from "./types.ts";
+import { existsSync, readFileSync, writeFileSync } from "fs";
+import type { Instance } from "./types.ts";
 
-export const getBuild = async (
-  channel: ReleaseChannel,
-  hash?: string,
-) => {
-  let build: { version_hash: string; html: string };
-  let commitHash;
-
-  if (channel === ReleaseChannel.STAGING) channel = ReleaseChannel.CANARY;
-
-  try {
-    const headers = { headers: { "accept": "application/vnd.github+json" } };
-    const commitsUrl = `${ENDPOINTS.RELEASE_CHANNELS_COMMITS}/${channel}/web`;
-
-    const commits = await (await fetch(commitsUrl, headers)).json() as Commit[];
-
-    if (!hash) commitHash = commits[0].sha;
-    else {
-      const commit = commits.find(({ commit }) =>
-        commit.message.includes(`${hash}`)
-      );
-
-      commitHash = commit?.sha;
-    }
-
-    const { content: info } = await (await fetch(
-      `${ENDPOINTS.RELEASE_CHANNELS}/${channel}/info.json?ref=${commitHash}`,
-    )).json();
-
-    const { content: html } = await (await fetch(
-      `${ENDPOINTS.RELEASE_CHANNELS}/${channel}/web/scripts/index.html?ref=${commitHash}`,
-    )).json();
-
-    build = {
-      version_hash: JSON.parse(atob(info)).version_hash,
-      html: atob(html),
-    };
-  } catch {
-    console.error(`Cannot connect to github, getting latest ${channel}`);
-
-    const html = await (await fetch(`${DOMAINS[channel]}/app`)).text();
-
-    const { hash } =
-      await (await fetch(`${DOMAINS[channel]}/assets/version.${channel}.json`))
-        .json();
-
-    build = { version_hash: hash, html };
+const checkForData = (): void => {
+  if (!existsSync("instances.json")) {
+    writeFileSync("instances.json", "[]");
   }
+};
 
-  return build as Build;
+export const getInstances = (): Instance[] => {
+  checkForData();
+
+  const file = readFileSync("instances.json", { encoding: "utf-8" });
+  const parsed = JSON.parse(file);
+
+  return parsed;
+};
+
+export const getInstance = (name: string): Instance | undefined => {
+  const instances = getInstances();
+
+  return instances.find((instance) => instance.name === name);
 };
