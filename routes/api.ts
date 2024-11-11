523aac94c835ecd961423bfbcf6119e5a098e109
CyberL1 <mateusz.hf18@gmail.com>
2024-11-11 12:19:48 +0100

    chore: remove unused code

--- routes/api.ts
+++ routes/api.ts
@@ -3,63 +3,57 @@ import { getInstance } from "../utils.ts";
 import { Domains } from "../types.ts";
 
 export default (fastify: FastifyInstance) => {
-  fastify.all(
-    "/*",
-    async (
-      req: FastifyRequest<{ Params: { "*": string } }>,
-      reply: FastifyReply,
-    ) => {
-      const instance = getInstance(req.headers.host?.split(".")[0]!);
+  fastify.all("/*", async (req: FastifyRequest, reply: FastifyReply) => {
+    const instance = getInstance(req.headers.host?.split(".")[0]!);
 
-      if (!instance) {
-        return reply.status(500).send({
-          error: "Cannot send api request",
-          reason: "Instance not found",
-        });
-      }
+    if (!instance) {
+      return reply.status(500).send({
+        error: "Cannot send api request",
+        reason: "Instance not found",
+      });
+    }
 
-      const instanceApiEndpoint = instance.endpoints.api
-        ? `https:${instance.endpoints.api.slice(0, -4)}`
-        : Domains[instance.releaseChannel];
+    const instanceApiEndpoint = instance.endpoints.api
+      ? `https:${instance.endpoints.api.slice(0, -4)}`
+      : Domains[instance.releaseChannel];
 
-      if (!instance.endpoints.api) {
-        req.headers.origin = "https://discord.com";
-      }
+    if (!instance.endpoints.api) {
+      req.headers.origin = "https://discord.com";
+    }
 
-      const responseOptions: RequestInit = {
-        method: req.method,
-        headers: req.headers as HeadersInit,
-      };
+    const responseOptions: RequestInit = {
+      method: req.method,
+      headers: req.headers as HeadersInit,
+    };
 
-      if (
-        !["HEAD", "GET"].includes(req.method) &&
-        req.headers["content-type"] === "application/json" &&
-        req.body
-      ) {
-        responseOptions.body = JSON.stringify(req.body);
-      }
+    if (
+      !["HEAD", "GET"].includes(req.method) &&
+      req.headers["content-type"] === "application/json" &&
+      req.body
+    ) {
+      responseOptions.body = JSON.stringify(req.body);
+    }
 
-      console.log(`${instanceApiEndpoint}${req.url}`);
+    console.log(`${instanceApiEndpoint}${req.url}`);
 
-      try {
-        const response = await fetch(
-          `${instanceApiEndpoint}${req.url}`,
-          responseOptions,
-        );
+    try {
+      const response = await fetch(
+        `${instanceApiEndpoint}${req.url}`,
+        responseOptions,
+      );
 
-        reply.status(response.status);
+      reply.status(response.status);
 
-        if (response.status != 204) {
-          const json = await response.json();
+      if (response.status != 204) {
+        const json = await response.json();
 
-          reply.send(json);
-        } else {
-          return;
-        }
-      } catch (err) {
-        console.log("API Proxy error:", err);
+        reply.send(json);
+      } else {
         return;
       }
-    },
-  );
+    } catch (err) {
+      console.log("API Proxy error:", err);
+      return;
+    }
+  });
 };
