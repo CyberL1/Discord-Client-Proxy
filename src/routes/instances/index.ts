978de6ee935ab92ba57d606586f512ad2e0eb0ad
CyberL1 <cyber.hf18@gmail.com>
2026-04-15 15:03:11 +0200

    feat: instance adding

--- src/routes/instances/index.ts
+++ src/routes/instances/index.ts
@@ -1,6 +1,22 @@
-import type { MethodRoutes } from "#src/types.ts";
-import { getInstances } from "#src/utils.ts";
+import type { FastifyRequest } from "fastify";
+import type { Instance, MethodRoutes } from "#src/types.ts";
+import { addInstance, getInstances, instanceBodySchema } from "#src/utils.ts";
 
 export default {
   GET: getInstances,
+  POST: {
+    schema: instanceBodySchema,
+    handler: (req: FastifyRequest<{ Body: Instance }>) => {
+      const instance: Instance = {
+        name: req.body.name.trim(),
+        endpoints: req.body.endpoints,
+        settings: {
+          releaseChannel: req.body.settings.releaseChannel,
+          useHttps: req.body.settings.useHttps,
+        },
+      };
+
+      return addInstance(instance);
+    },
+  },
 } satisfies MethodRoutes;
