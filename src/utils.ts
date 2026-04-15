39bbf2be0e8da30df3f0828728e08e43beb2df1b
CyberL1 <cyber.hf18@gmail.com>
2026-04-15 15:03:11 +0200

    feat: instance adding

--- src/utils.ts
+++ src/utils.ts
@@ -33,3 +33,53 @@ export const getInstance = (name: string): Instance => {
 
   return instance;
 };
+
+export const addInstance = (instance: Instance): Instance => {
+  const instances = getInstances();
+
+  if (
+    instances.find(
+      ({ name }) => normalizeName(name) === normalizeName(instance.name),
+    )
+  ) {
+    throw new Error("Instance already exists");
+  }
+
+  instances.push(instance);
+  writeFileSync(instancesFilePath, JSON.stringify(instances));
+
+  return instance;
+};
+
+export const instanceBodySchema = {
+  body: {
+    type: "object",
+    properties: {
+      name: { type: "string", minLength: 1 },
+      endpoints: {
+        type: "object",
+        properties: {
+          api: { type: "string", minLength: 1 },
+          gateway: { type: "string", minLength: 1 },
+          cdn: { type: "string", minLength: 1 },
+          media: { type: "string", minLength: 1 },
+        },
+        additionalProperties: false,
+      },
+      settings: {
+        type: "object",
+        properties: {
+          releaseChannel: {
+            type: "string",
+            pattern: "^stable|ptb|canary|staging$",
+          },
+          useHttps: { type: "boolean" },
+        },
+        additionalProperties: false,
+        required: ["releaseChannel"],
+      },
+    },
+    additionalProperties: false,
+    required: ["name"],
+  },
+};
